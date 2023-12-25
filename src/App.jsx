4236961b05356ef66e5550d4fd8918a3a1dbbf27
anshul-59 <anshul.bansod5901@gmail.com>
2023-12-25 16:36:58 +0530

    Defined a new component 'TimerChallenge' used basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Eeezee" targetTime={1}/>
+        <TimerChallenge title="Kinda Easy" targetTime={5}/>
+        <TimerChallenge title="Difficult" targetTime={10}/>
+        <TimerChallenge title="I don't think you can do it" targetTime={15}/>
+      </div>
     </>
   );
 }
