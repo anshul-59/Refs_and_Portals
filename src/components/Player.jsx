440212516b8b510123cece78c4b73aac0b2e7729
anshul-59 <anshul.bansod5901@gmail.com>
2023-12-25 11:27:22 +0530

    Using ref to read a value and output it only when the button is clicked.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
 
+  const playerName = useRef();
+  
   const [enteredPlayerName, setEnteredPlayerName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+  // const [submitted, setSubmitted] = useState(false);
 
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
+  // function handleChange(event) {
+  //   setSubmitted(false);
+  //   setEnteredPlayerName(event.target.value);
+  // }
   function handleClick() {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : 'Random Player'}</h2>
+      <h2>Welcome {enteredPlayerName ? enteredPlayerName : 'Random Player'}</h2>
       <p>
-        <input onChange={handleChange} type="text" value={enteredPlayerName} />
-        <button  onClick={handleClick}>Set Name</button>
+        <input 
+          ref={playerName}
+          type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
 }
+
+  
+
+
